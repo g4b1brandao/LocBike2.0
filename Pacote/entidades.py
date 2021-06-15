6dd3494eac413d51f7d2af2eacacd905ddfc5500
Ana Gabrielle <ana.gabrielle.brandao62@aluno.ifce.edu.br>
2021-06-15 20:44:20 +0000

    Modificações, cadastro e catálogo funcional,criação da tabela de aluguel

--- Pacote/entidades.py
+++ Pacote/entidades.py
@@ -1,5 +1,7 @@
 from Pacote import db, loginmanager
 from flask_login import UserMixin
+from datetime import datetime
+
 
 
 class usuario(db.Model, UserMixin):
@@ -19,7 +21,22 @@ class cadastrodebikes(db.Model):
     CEP = db.Column(db.String(9), nullable = False)
     Model = db.Column(db.String(100), nullable = False)
     Modality = db.Column(db.String(20), nullable = False)
-    Aro_e_Machas = db.Column(db.String(20), nullable = False)
+    Gender = db.Column(db.String(20), nullable = False)
+    Suspensao = db.Column(db.String(30), nullable = False)
+    Aro = db.Column(db.String(20), nullable = False)
+    Marchas = db.Column(db.String(20), nullable = False)
+    Marca = db.Column(db.String(30), nullable = False)
+    Price = db.Column(db.String(20), nullable = False)
     img_bicicleta = db.Column(db.String(100), default="")
 
     id_usuario = db.Column(db.Integer, db.ForeignKey("usuario.id"), nullable = False)
+    alugueldebikes = db.relationship('alugueldebikes', backref='cadastrodebike', lazy=True)
+
+class alugueldebikes(db.Model):
+     id = db.Column(db.Integer, primary_key = True)
+     DataRetirada = db.Column(db.DateTime(20), nullable = False)
+     HoraRetirada = db.Column(db.DateTime(20), nullable = False)
+     DataDevolucao =db.Column(db.DateTime(20), nullable = False)
+     HoraRetirada =db.Column(db.DateTime(20), nullable = False)
+
+     id_cadastrodebikes = db.Column(db.Integer, db.ForeignKey("cadastrodebikes.id"), nullable = False)
--- Pacote/routes.py
+++ Pacote/routes.py
@@ -1,11 +1,12 @@
-from flask import current_app as app, render_template, request,redirect, session, url_for
+from flask import current_app as app, render_template, request,redirect, session, send_from_directory
 from Pacote import db, loginmanager
 import os
 from werkzeug.utils import secure_filename
-from Pacote.entidades import usuario , cadastrodebikes
+from Pacote.entidades import usuario , cadastrodebikes, alugueldebikes
 
 from flask_login import login_user, logout_user, login_required,current_user
 
+from datetime import datetime
 
 loginmanager.login_view = '/login'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
@@ -14,66 +15,23 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-bic_mock = [
-    {
-        "title":"Bicicleta de teste1",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste2",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste3",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste4",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste5",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste6",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste7",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste8",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste9",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste10",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste11",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-    {
-        "title":"Bicicleta de teste12",
-        "image":"https://static.netshoes.com.br/produtos/bicicleta-de-passeio-kls-retro-aro-26-com-freios-v-brake/54/PAK-0007-154/PAK-0007-154_zoom1.jpg?ts=1616414207&"
-    },
-]
-
-
 @app.route("/")
 def inicio():
 	return render_template("index.html")
-@app.route("/catalogo")
+
+@app.route('/imagens/<path:filename>')
+@login_required
+def base_static(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+@app.route("/catalogo", methods=['GET'])
 @login_required
 def catalogo():
     bicicletas=[1,2,3,4,5,6,7]
-    return render_template("catalogo.html", bicicletas=bicicletas, bic_mock=bic_mock)
+    dados = cadastrodebikes.query.all()
+    return render_template("catalogo.html", bicicletas=bicicletas,dados=dados)
+
+
 
 @app.route("/cadastro" , methods=['GET', 'POST']) # Adicionei método GET
 def cadastro():
@@ -128,9 +86,14 @@ def cadastrodebicicletas():
         cidade = request.form ['City']
         estado = request.form ['State']
         cep = request.form ['CEP']
-        modelo = request.form ['Model']
+        nomedabicicleta = request.form ['Model']
         modalidade = request.form ['Modality']
-        aro_e_machas = request.form ['Aro_e_Machas']
+        genero = request.form ['Gender']
+        suspensao = request.form ['Suspensao']
+        aro = request.form ['Aro']
+        marchas = request.form ['Marchas']
+        marca = request.form ['Marca']
+        preco = request.form ['Location']
 
         img_bicicleta = ''
         if 'file' not in request.files:
@@ -153,9 +116,14 @@ def cadastrodebicicletas():
         novo.City =  cidade
         novo.State = estado
         novo.CEP = cep
-        novo.Model = modelo
+        novo.Model = nomedabicicleta
         novo.Modality = modalidade
-        novo.Aro_e_Machas = aro_e_machas
+        novo.Gender = genero
+        novo.Suspensao = suspensao
+        novo.Aro = aro
+        novo.Marchas = marchas
+        novo.Marca = marca
+        novo.Price = preco
         novo.img_bicicleta = img_bicicleta
         novo.id_usuario = current_user.id
 
@@ -163,19 +131,10 @@ def cadastrodebicicletas():
         db.session.add(novo)
         db.session.commit()
 
-        return redirect('/teste2')
+        return redirect('/catalogo')
     return render_template("cadastrodebicicletas.html")
 
 
-
-
-@app.route("/teste", methods=['POST'])
-def teste():
-    return render_template("teste.html")
-
-@app.route("/teste2")
-def teste2():
-    return render_template("teste2.html")
 
 @app.route('/bikesdepasseios')
 @login_required
@@ -190,12 +149,38 @@ def bikescasuais():
 @app.route('/bikesesportivas')
 @login_required
 def bikesesportivas():
-       return render_template("bikesesportivas.html")
+     return render_template("bikesesportivas.html")
 
 @app.route('/barralateral')
 def barralateral():
        return render_template("barralateral.html")
 
+
+@app.route('/alugar', methods=['GET', 'POST'])
+@login_required
+def alugar():
+     if request.method == 'POST':
+        DataRetirada = request.form ['DataRetirada']
+        HoraRetirada = request.form ['HoraRetirada']
+        DataDevolucao = request.form ['DataDevoluçao']
+        HoraRetirada= request.form ['HoraRetirada']
+
+
+        novo = alugueldebikes()
+        novo.DataRetirada = DataRetirada
+        novo.HoraRetirada = HoraRetirada
+        novo.DataDevolucao =  DataDevolucao
+        novo.HoraRetirada = HoraRetirada
+
+        novo.id_cadastrodebikes = current_user.id
+
+
+        db.session.add(novo)
+        db.session.commit()
+
+        return redirect('/catalogo')
+
+     return render_template("alugar.html")
 
 
 @app.route('/logout')
